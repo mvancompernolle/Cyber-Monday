680bd3243d117397899d418e82a17fbb7de16dfe
Matthew Van Compernolle <matthew.vancompernol@sea.aie.edu>
2016-01-31 09:47:09 -0800

    bad gury

--- Assets/Scripts/CreepScript.cs
+++ Assets/Scripts/CreepScript.cs
@@ -5,11 +5,15 @@ public class CreepScript : MonoBehaviour {
     GameObject player;
     Rigidbody2D rbody;
     bool leaping = false;
+    bool walking = false;
     float leapSpeed = 2.0f;
     Vector2 leapDirection;
-    float leapDistance = 1.5f;
+    float leapDistance = 1.0f;
+    float walkingRange = 1.5f;
+    float leapingRange = 0.75f;
     float currLeapDist = 0.0f;
     float leapCooldown = 0.5f, currLeapDt = 0.0f;
+    float walkSpeed = 0.25f;
 
 	// Use this for initialization
 	void Start () {
@@ -30,17 +34,23 @@ public class CreepScript : MonoBehaviour {
 
         if (!leaping)
         {
+            transform.rotation = Quaternion.identity;
+            Vector2 dir = player.transform.position - transform.position;
+            float playerAngle = Mathf.Rad2Deg * Mathf.Atan2(toPlayer.y, toPlayer.x) - 90;
+            transform.Rotate(0.0f, 0.0f, playerAngle);
 
-            if (toPlayer.magnitude <= 1.0f)
+            if (Vector2.Dot(myDir, playerDir) > 0.5f)
             {
-                transform.rotation = Quaternion.identity;
-                Vector2 dir = player.transform.position - transform.position;
-                float playerAngle = Mathf.Rad2Deg * Mathf.Atan2(toPlayer.y, toPlayer.x) - 90;
-                transform.Rotate(0.0f, 0.0f, playerAngle);
-
-                if (Vector2.Dot(myDir, playerDir) > 0.5f && !leaping && currLeapDt <= 0.0f)
+                if (toPlayer.magnitude <= walkingRange && toPlayer.magnitude > leapingRange)
+                {
+                    transform.Translate(toPlayer * walkSpeed * Time.deltaTime, Space.World);
+                    walking = true;
+                    leaping = false;
+                }
+                else if (toPlayer.magnitude <= leapingRange && !leaping && currLeapDt <= 0.0f)
                 {
                     leaping = true;
+                    walking = false;
                     leapDirection = toPlayer.normalized;
                     currLeapDist = leapDistance;
                     Debug.Log(toPlayer);
